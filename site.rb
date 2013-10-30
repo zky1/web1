ef2096a0a3390c7faee32a6b8aa2d61d3abca09c
Sybil C. K. Wong <sybil.wong@hotmail.co.uk>
2013-10-30 14:44:51 +0000

    Added Twitter Bootstrap stylesheet

--- site.rb
+++ site.rb
@@ -2,7 +2,11 @@ require 'sinatra' #downloads and installs program inside Ruby
 get '/' do #when user gets to index page '/' #get is what user gets #do opens sequence of instructions
 	erb :index #tells Ruby to replace yield in layout.erb with index.erb contents
 end #end closes do sequence
- 
+
+get '/events' do #page 6: create other URLs
+	erb :events
+end
+
 get '/italian' do #page 6: create other URLs
 	"ciao" #print string
 end
