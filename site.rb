b2b769043c233bc03ae4e6c2439d9db991a1d928
Sybil CK Wong <sybil.wong@hotmail.co.uk>
2013-11-24 23:53:50 +0000

    Added index2.erb for testing

--- site.rb
+++ site.rb
@@ -1,10 +1,11 @@
 require 'sinatra' #downloads and installs program inside Ruby
+
 get '/' do #when user gets to index page '/' #get is what user gets #do opens sequence of instructions
 	erb :index #tells Ruby to replace yield in layout.erb with index.erb contents
 end #end closes do sequence
 
-get '/events' do #page 6: create other URLs
-	erb :events
+get '/index2' do #page 6: create other URLs
+	erb :index2
 end
 
 #post is for sending data to server
