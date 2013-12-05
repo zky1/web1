6fbf5f9178b97159f2e1fbe5f90bd49516388aba
Sybil CK Wong <sybil.wong@hotmail.co.uk>
2013-12-05 13:52:35 +0000

    Added signup.erb

--- site.rb
+++ site.rb
@@ -4,8 +4,12 @@ get '/' do #when user gets to index page '/' #get is what user gets #do opens se
 	erb :index #tells Ruby to replace yield in layout.erb with index.erb contents
 end #end closes do sequence
 
-get '/index2' do #page 6: create other URLs
+get '/index2' do
 	erb :index2
+end
+
+get '/signup' do
+	erb :signup
 end
 
 #post is for sending data to server
